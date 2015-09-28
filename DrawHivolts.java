11c9940306953be1a2ff7d136b7d6e4dad5f0eae
bourbonnais <csybourbonnais@gmail.com>
2015-09-28 11:43:47 -0700

    Update DrawHivolts.java

--- DrawHivolts.java
+++ DrawHivolts.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 //September 15, 2015 
 
 public class DrawHivolts extends JFrame{
+	private final Color YELLOW = new Color(0xEE9605);
+
 	//screen refers to the pop-up window 
 	 int screenW;
 	 int screenH;
@@ -22,8 +24,10 @@ public class DrawHivolts extends JFrame{
 	 int canvasOx = 0;
 	 int canvasOy = 22;
 
-	 
-	 
+	 Character you = new Character(randomXCoord(), randomYCoord());
+	 //Character mho1 = new Character(randomXCoord(), randomYCoord());
+	 Character mho[] = new Character[12];
+	 	 
 	public DrawHivolts() {
 		init();
 	}
@@ -36,19 +40,19 @@ public class DrawHivolts extends JFrame{
 		//set screen width and height to actual values of window
 		 screenW = getWidth();
 		 screenH = getHeight();
-	
 	}
 	
 	public void paint(Graphics g){
 		g.setColor(Color.BLACK);
 		g.fillRect(canvasOx, canvasOy, canvasW, canvasH);
 		drawGrid(g);
-		
-		g.setColor(Color.CYAN);
-		g.fillOval(randomXCoord(), randomYCoord(), 40, 40);
-		
-		g.setColor(Color.MAGENTA);
-		g.fillOval(randomXCoord(), randomYCoord(), 40, 40);
+		// draw smileys
+		initializeMhos(mho);
+		for (int i = 0; i < mho.length; i++) {
+			drawSmiley(g, mho[i].getXCoord(), mho[i].getYCoord(), YELLOW, Color.BLACK, 0, 25);
+		}
+		drawSmiley(g, you.getXCoord(), you.getYCoord(), Color.BLACK, YELLOW, 180, 22);
+		drawFrences(g);
 	}
 	
 	public void drawGrid(Graphics g){
@@ -56,42 +60,77 @@ public class DrawHivolts extends JFrame{
 		g.setColor(Color.ORANGE);
 		for(int i = 0; i < 13; i++){
 			//vertical
-			g.drawLine(Gridx2Screenx(i), 0, Gridx2Screenx(i), GridHeight2ScreenHeight(GRIDH));
+			g.drawLine(Gx2Sx(i), 0, Gx2Sx(i), Gh2Sh(GRIDH));
 			//horizontal
-			g.drawLine(0, Gridy2Screeny(i), GridWidth2ScreenWidth(GRIDW), Gridy2Screeny(i));
+			g.drawLine(0, Gy2Sy(i), Gw2Sw(GRIDW), Gy2Sy(i));
 		}
 	
 	}
 	
-	 public int Gridx2Screenx (double gx) {
+	public void drawSmiley(Graphics g, int x, int y, Color Color1, Color Color2, int degree, int mouth) {
+		g.setColor(Color1);
+		g.fillOval(x, y, 40, 40);
+		g.setColor(Color2);
+		g.drawOval(x, y, 40, 40);
+		g.fillOval(x+10, y+10, 5, 10);
+		g.fillOval(x+25, y+10, 5, 10);
+		g.drawArc(x+10, y+mouth, 20, 10, degree, 180);
+	}
+	
+	public void drawFrences(Graphics g) {
+		int x = 0; int y = 0;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 12; j++) {
+				drawSmiley(g, Gx2Sx(x), Gy2Sy(y), Color.BLACK, YELLOW, 180, 22);
+				x++;
+			}
+			y = 11; x = 0;
+		}
+		y = 1;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 10; j++) {
+				drawSmiley(g, Gx2Sx(x), Gy2Sy(y), Color.BLACK, YELLOW, 180, 22);
+				y++;
+			}
+			y = 1; x = 11;
+		}
+	}
+	
+	 public int Gx2Sx (double gx) {
 			int sX =(int)( (gx / GRIDW) * canvasW + canvasOx); //convert to canvas coordinates, add canvas offset
 			return sX;
 		}
 	 
-	 public int Gridy2Screeny (double gy) {
+	 public int Gy2Sy (double gy) {
 			int sY =(int)( (gy / GRIDH) * canvasH + canvasOy); //convert to canvas coordinates, add canvas offset
 			return sY;
 		}
 	 
-	 public int GridWidth2ScreenWidth (double gW){
+	 public int Gw2Sw (double gW){
 		 int sW =(int)( (gW) * (canvasW / GRIDW));
 		 return sW;
 	 }
 	 
-	 public int GridHeight2ScreenHeight(double gH){
+	 public int Gh2Sh(double gH){
 		 int sH =(int)( (gH) * (canvasH / GRIDH) + canvasOy);
 		 return sH;
 	 }
 	 
 	 public int randomXCoord(){
 		 int randomCoord = (int)(1 + Math.random() * 10);
-		 randomCoord = Gridx2Screenx(randomCoord);
+		 randomCoord = Gx2Sx(randomCoord);
 		 return randomCoord;
 	 }
 	 
 	 public int randomYCoord(){
 		 int randomCoord = (int)(1 + Math.random() * 10);
-		 randomCoord = Gridy2Screeny(randomCoord);
+		 randomCoord = Gy2Sy(randomCoord);
 		 return randomCoord;
 	 } 
+	 
+	 public void initializeMhos(Character array[]) {
+		 for (int i = 0; i < array.length; i++) {
+			 array[i] = new Character(randomXCoord(), randomYCoord());
+		 }
+	 }
 }
