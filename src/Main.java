343c3a2799ee7bc72323990e1b0bc6d7795e2c15
cdrebin <cd12403@pausd.us>
2015-10-12 15:31:33 -0700

    Update references
    update HivoltsGameState references

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 
 public class Main{
 	public static void main (String[] args){
-		HivoltsGame h = new HivoltsGame();
+		HivoltsGameState h = new HivoltsGameState();
 		h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		h.setVisible(true);
 	}
